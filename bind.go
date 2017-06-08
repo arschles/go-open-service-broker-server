785917915bf477419d1d92c432ce5c537411b86a
Aaron Schlesinger <aaschles@microsoft.com>
2017-06-08 16:20:49 -0700

    adjusting lines & adding (skeleton) bind

---
+++ bind.go
@@ -0,0 +1,11 @@
+package osbserver
+
+import (
+	"net/http"
+)
+
+func bind(ops Operations) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
--- server.go
+++ server.go
@@ -14,13 +14,25 @@ func Run(port int, operations Operations) error {
 	r.HandleFunc("/v2/catalog", getCatalog(operations)).Methods("GET")
 
 	// poll last operation
-	r.HandleFunc("/v2/service_instances/{instance_id}/last_operation", pollLastOperation(operations)).Methods("GET")
+	r.HandleFunc(
+		"/v2/service_instances/{instance_id}/last_operation",
+		pollLastOperation(operations),
+	).Methods("GET")
 
 	// provision
 	r.HandleFunc("/v2/service_instances/{instance_id}", provision(operations)).Methods("PUT")
 
 	// update service instance
-	r.HandleFunc("/v2/service_instances/{instance_id}", updateInstance(operations)).Methods("PATCH")
+	r.HandleFunc(
+		"/v2/service_instances/{instance_id}",
+		updateInstance(operations),
+	).Methods("PATCH")
+
+	// bind
+	r.HandleFunc(
+		"/v2/service_instances/{instance_id}/service_bindings/{binding_id}",
+		bind(operations),
+	).Methods("PUT")
 
 	hostStr := fmt.Sprintf(":%d", port)
 	http.ListenAndServe(hostStr, r)
